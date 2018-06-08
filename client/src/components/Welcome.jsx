cd5bbc3748ded2d7abbf662bfa9e32e472cececa
Will Ruiz <thisis.willruiz@gmail.com>
2018-06-08 14:19:36 -0700

    Request username and update parent state

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 
 export default class Battlefield extends Component {
+  state = {
+    newUsername: ''
+  }
+  handleInput = (event) => {
+    let { value } = event.target;
+    this.setState({newUsername: value});
+  }
   render() {
+    const { addUsername } = this.props;
     return(
-      <div>Welcome to BattleShipt.</div>
+      <div>
+        <h1>Welcome to BattleShipt.</h1>
+        <div>
+        <form>
+          <label>
+            Your name, General:
+            <input type="text" value={this.state.newUsername}
+              onChange={this.handleInput} />
+          </label>
+          <input type="button" value="Submit" onClick={addUsername} />
+        </form>
+        </div>
+      </div>
     );
   }
 }
