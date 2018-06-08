9db12fa45ea1e95f6ef65638d6c8d897a811db6c
Will Ruiz <thisis.willruiz@gmail.com>
2018-06-07 19:10:16 -0700

    Initialize Battlefield component

---
+++ client/src/components/Battlefield.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Battlefield extends Component {
+  render() {
+    return(
+      <div>Welcome to the battlefield.</div>
+    );
+  }
+}
