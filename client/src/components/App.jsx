3ddd7ddc011f2fc1812bd5f28c73179d4b800e80
Will Ruiz <thisis.willruiz@gmail.com>
2018-06-07 18:58:30 -0700

    Conditionally render component based on username state

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { Redirect } from 'react-router';
+
+import Welcome from './Welcome.jsx';
+import Battlefield from './Battlefield.jsx';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: ''
+    }
+  }
   render () {
     return (
-      <div>Something great goes here.</div>
+      <div>
+      {
+        this.state.username ?
+          <Redirect to="/battle" /> :
+          <Redirect to="/" />
+      }
+      <Route exact="exact" path="/" component={Welcome} />
+      <Route exact="exact" path="/battlefield" component={Battlefield} />
+      </div>
     );
   }
 }
