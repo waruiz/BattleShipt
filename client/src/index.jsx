ac743577b1f1c11b8f9e80f1c3c35506d62578ba
Will Ruiz <thisis.willruiz@gmail.com>
2018-06-07 19:01:46 -0700

    Wrap App in Router for component routing

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,7 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './components/App.jsx';
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<Router><App/></Router>, document.getElementById('app'));
