c19bf6f7083ef37abc7a075c53a347572d2f5bf3
Will Ruiz <thisis.willruiz@gmail.com>
2018-06-07 22:12:49 -0700

    [fix] Resolve all client-side URL paths using server-side wildcard

--- server/index.js
+++ server/index.js
@@ -6,5 +6,7 @@ const http = require('http').Server(app);
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, '../client/dist/')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../client/dist/index.html')));
 
 app.listen(3000, () => console.log(`Listening on port 3000`));
