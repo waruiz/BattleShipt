885c49412c724635ea6976a4f3785512ac234057
Will Ruiz <thisis.willruiz@gmail.com>
2018-06-09 00:31:40 -0700

    [fix] Render all tiles in proper order

--- client/src/components/Grid.jsx
+++ client/src/components/Grid.jsx
@@ -8,15 +8,15 @@ export default class Grid extends Component {
 
     let columnStart = 1;
     let columnEnd = 2;
-    let rowStart = 0;
-    let rowEnd = 1;
+    let rowStart = 1;
+    let rowEnd = 2;
     
-    for (let i = 0; i < 25; i++) {
+    for (let i = 1; i <= 25; ++i) {
       let position = {
         gridArea: `${rowStart} / ${columnStart} / ${rowEnd} / ${columnEnd}`
       };
 
-      if (i % 5 === 0) {
+      if ((i > 1) && (i % 5 === 0)) {
         rowStart += 1;
         rowEnd += 1;
         columnStart = 1;
@@ -26,13 +26,13 @@ export default class Grid extends Component {
         columnEnd += 1;
       }
       tiles.push(
-        <div style={position} >{i + 1}</div>
+        <div style={position} >{i}</div>
       );
     }
 
     return(
       <div id="ocean" className='gridSize' >
-        {tiles.map(tile => tile)}
+        {tiles}
       </div>
     );
   }
