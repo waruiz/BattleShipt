a4cfd5bd37b2c963beadf6337e5fa7d858083bfd
Will Ruiz <thisis.willruiz@gmail.com>
2018-06-09 23:22:21 -0700

    Style Battlefield headline

--- client/src/components/Battlefield.jsx
+++ client/src/components/Battlefield.jsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 import Grid from './Grid.jsx';
 
+import style from '../styles/Battlefield.css';
+
 export default class Battlefield extends Component {
   render() {
     return(
       <div>
-        <h1>Welcome to the battlefield.</h1>
+        <h1 className='headline' >Welcome to the battlefield</h1>
         <Grid />
       </div>
     );
