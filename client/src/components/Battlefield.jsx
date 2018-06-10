6ef11154fd6fada30a6682588d6f6be25fe6e623
Will Ruiz <thisis.willruiz@gmail.com>
2018-06-10 15:39:29 -0700

    Pass props

--- client/src/components/Battlefield.jsx
+++ client/src/components/Battlefield.jsx
@@ -8,7 +8,7 @@ export default class Battlefield extends Component {
   render() {
     return(
       <div>
-        <h1 className='headline' >Welcome to the battlefield</h1>
+        <h1 className='headline' >Welcome to the battlefield {this.props.username}</h1>
         <Grid />
       </div>
     );
