751902094a2cfcebd218f7eb80a567e17afc39b3
Will Ruiz <thisis.willruiz@gmail.com>
2018-06-10 14:41:05 -0700

    Create Context API's Provider component, store test state, wrap App in Provider

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './components/App.jsx';
+import Context from './components/Context.jsx';
 
-ReactDOM.render(<Router><App/></Router>, document.getElementById('app'));
+class Provider extends Component {
+  state = {
+    username: 'William'
+  }
+  addUsername = (username) => {
+    this.setState({username});
+  }
+  render() {
+    return (
+      <Context.Provider value={this.state} >
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Provider>
+    <Router><App /></Router>  
+  </Provider>
+  , document.getElementById('app')
+);
