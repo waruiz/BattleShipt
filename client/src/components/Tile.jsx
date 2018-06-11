12decacf41d18c3cc931614228f4f12746ae5cae
Will Ruiz <thisis.willruiz@gmail.com>
2018-06-10 17:33:49 -0700

    Conditionally render ship or explosion, based on startGame state

--- client/src/components/Tile.jsx
+++ client/src/components/Tile.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 
+import Context from './Context.jsx';
+
 import style from '../styles/Tile.css';
 import ship from '../assets/ship_small.png';
+import explosion from '../assets/explosion_small.png';
 
 export default class Tile extends Component {
   state = {
@@ -17,8 +20,17 @@ export default class Tile extends Component {
     return (
       <div className='tileStyle' style={this.props.position}
         onClick={this.selectTile} id={`coordinate-${this.props.i}`} >
-      {this.state.selected ? <span><img src={ship} className='battleship' /></span>
-          : <span></span>}
+      <Context.Consumer>
+      {({startGame}) => (
+        startGame === false ?
+          (this.state.selected ?
+            <span><img src={ship} className='tilePiece' /></span>
+            : <span></span>)
+        : (this.state.selected === true ?
+            <span><img src={explosion} className='tilePiece' /></span>
+            : <span></span>)
+      )}
+      </Context.Consumer>
       </div>
     );
   }
