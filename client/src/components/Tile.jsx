2c197839ce405c80c9ad18854fa8e892cf31de33
Will Ruiz <thisis.willruiz@gmail.com>
2018-06-11 00:59:03 -0700

    [fix] Conditionally render correct piece image based on state of startGame, shipPlaced, and selected

--- client/src/components/Tile.jsx
+++ client/src/components/Tile.jsx
@@ -12,9 +12,14 @@ export default class Tile extends Component {
     shipPlaced: false,
   }
 
-  selectTile = (e) => {
+  selectTile = (gameStatus) => {
     let opposite = !(this.state.selected);
-    this.setState({selected: opposite});
+    if (!gameStatus) {
+      let myShip = !(this.state.shipPlaced);
+      this.setState({selected: opposite, shipPlaced: myShip});
+    } else {
+      this.setState({selected: opposite});
+    }
   }
   
   render () {
@@ -23,15 +28,16 @@ export default class Tile extends Component {
         {({startGame}) => (
           <React.Fragment>
             <div className='tileStyle' style={this.props.position}
-              onClick={this.selectTile} id={`coordinate-${this.props.i}`} >
+              onClick={() => this.selectTile(startGame)} id={`coordinate-${this.props.i}`} >
             {
             startGame === false ?
               (this.state.selected ?
                 <span><img src={ship} className='tilePiece' /></span>
                 : <span></span>)
-            : (this.state.selected === true ?
+            : (this.state.selected && this.state.shipPlaced === false ?
                 <span><img src={explosion} className='tilePiece' /></span>
-                : <span></span>)
+                : (this.state.selected && this.state.shipPlaced ?
+                  <span><img src={ship} className='tilePiece' /></span> : <span></span>))
             }
             </div>
           </React.Fragment>
