ca63dcfe68a6b9115e70971aee0a8c514cdb4fbf
Will Ruiz <thisis.willruiz@gmail.com>
2018-06-11 19:51:53 -0700

    Conditionally render strike type message

--- client/src/components/ScoreBoard.jsx
+++ client/src/components/ScoreBoard.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 
+import Context from './Context.jsx';
+
 export default class ScoreBoard extends Component {
   render() {
     return(
-      <div>This is the ScoreBoard.</div>
+      <div>
+      <Context.Consumer>
+      {({userStrikeType}) => (
+        userStrikeType !== null ?
+          <p>Most recent strike {userStrikeType}</p>
+          : <p>Play your first strike on the board.</p>
+      )}
+      </Context.Consumer>
+      </div>
     );
   }
 }
