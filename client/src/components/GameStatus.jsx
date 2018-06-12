2c17905ea28e53996aa3df4dc5132a0cee2a0b58
Will Ruiz <thisis.willruiz@gmail.com>
2018-06-11 19:35:16 -0700

    Notify user they are playing Computer

--- client/src/components/GameStatus.jsx
+++ client/src/components/GameStatus.jsx
@@ -8,12 +8,12 @@ export default class GameStatus extends Component {
     return(
       <div>
       <Context.Consumer>
-      {({startGame, opponentStartGame}) => (
+      {({startGame, opponentStartGame, opponentName}) => (
         !startGame && !opponentStartGame ?
           <span>Waiting on both players to start game. <StartGameButton /></span>
           : (startGame ?
             <span>Waiting on opponent to start game.</span>
-            : <span>Opponent ready. Waiting on you to start game. <StartGameButton /></span>)
+            : <span>{opponentName} ready. Waiting on you to start game. <StartGameButton /></span>)
       )}
       </Context.Consumer>
       </div>
